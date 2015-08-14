845b4e0e41d15653995bae3dc737c1856489333c
Bonnie <aalle.allen@gmail.com>
2015-08-14 01:37:10 -0400

    Some design and editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,24 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.all.order('created_at DESC')
+	@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
 	end
 
-	def create
-		@post = Post.new(post_params)
-		@post.save
-
-		redirect_to @post
+	def show
+	@post = Post.find(params[:id])
 	end
 
-	def show
-		@post = Post.find(params[:id])
+	def create
+	@post = Post.new(post_params)
+	@post.save
+
+	redirect_to @post
 	end
 
 	private
 	def post_params
-		params.require(:post).permit(:title, :body)
+	params.require(:post).permit(:title, :body)
 	end
 end
